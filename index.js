894f1f627e2e57c0c2d3b78986297d3059ebb0b2
IconicLuck <uhasib959@gmail.com>
2023-05-19 16:46:17 +0600

    implemented delete a toy from the server

--- index.js
+++ index.js
@@ -70,6 +70,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/toyCars/:id', async (req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCarsCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
 
 
